c351378b1a081fcaa00b59bb5fe30f96dd65473d
ianirudhkhabya <ianirudhkhabya@gmail.com>
2023-06-18 07:43:18 +0530

    feat: addnote route added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -15,4 +15,40 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
+// Route 2: Add a new note using: POST "/api/notes/addnote". Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Title should be valid!").isLength({ min: 3 }),
+    body("description", "Description must be at least 3 words").isLength({
+      min: 7,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { title, description, tag } = req.body;
+      if (!title || !description) {
+        return res.status(400).json({ error: "Please add all the fields" });
+      }
+      const note = new Note({
+        user: req.user.id,
+        title,
+        description,
+        tag,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Server Error" });
+    }
+  }
+);
+
 module.exports = router;
