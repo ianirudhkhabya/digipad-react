af20ca686c4ff172b917bfcffbbc86b544deb73e
ianirudhkhabya <ianirudhkhabya@gmail.com>
2023-06-18 07:41:22 +0530

    feat: fetchallnotes route added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const Note = require("../models/Note");
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  obj = {
-    c: "this is c",
-    d: "this is d",
-  };
-  res.json(obj);
+// Route 1: Get all the notes using: GET "/api/notes/fetchallnotes". Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
 });
 
 module.exports = router;
