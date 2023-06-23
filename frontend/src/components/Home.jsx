e6c49a1ef6bd47ca32c0406062e3dd4da9d49f92
ianirudhkhabya <ianirudhkhabya@gmail.com>
2023-06-23 11:02:49 +0530

    created noteItem component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,7 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import React from "react";
+import Notes from "./Notes";
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const { notes, setNotes } = context;
-
   return (
     <>
       <div className="container my-3">
@@ -36,13 +33,7 @@ const Home = () => {
           </button>
         </form>
       </div>
-
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note) => {
-          return note.title;
-        })}
-      </div>
+      <Notes />
     </>
   );
 };
